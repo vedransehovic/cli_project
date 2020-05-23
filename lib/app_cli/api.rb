48d8c7850760731a842b85b689c5136a36fac353
vedransehovic <vedran.sehovic@gmail.com>
2020-05-23 15:07:34 -0400

    WPI: added a method to API class

--- lib/app_cli/api.rb
+++ lib/app_cli/api.rb
@@ -1,4 +1,14 @@
 class Api
     puts "Api class loaded"
+    BASE_URL = "https://breakingbadapi.com/api/characters" #this is API's base URL, it's constant because... well it doesn't change. 
 
+    def self.get_name
+        uri = URI.parse(BASE_URL)
+        response = Net::HTTP.get_response(uri)
+        body = response.body
+        json =JSON.parse(body)
+        ap json
+    end
 end
+
+Api.get_name
